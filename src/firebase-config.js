e8785b4295ed25a40b4d6f83418faa1672693697
JustbeV <maxgamnam@gmail.com>
2026-04-02 23:01:34 +0800

    new added

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC_T9ZG3lgd5VQMI5SAEqwk1duK3dZ5beQ",
+  authDomain: "mini-store-app-a98af.firebaseapp.com",
+  projectId: "mini-store-app-a98af",
+  storageBucket: "mini-store-app-a98af.firebasestorage.app",
+  messagingSenderId: "1077754476883",
+  appId: "1:1077754476883:web:9874e16451c7e31a8689a6"
+};
